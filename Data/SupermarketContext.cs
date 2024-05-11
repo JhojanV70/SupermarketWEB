6431dba9bc634b6d78aedea2099c35ac7647d80d
JhojanV70 <jhojanv70@gmail.com>
2024-05-11 16:35:12 -0500

    Index Categories

--- Data/SupermarketContext.cs
+++ Data/SupermarketContext.cs
@@ -9,5 +9,11 @@ namespace SupermarketWEB.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=SupermarketEF;Trusted_Connection=True;");
+        }
     }
 }
