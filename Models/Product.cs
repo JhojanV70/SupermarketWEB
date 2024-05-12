b26bd2dbd931ac454b279a8565c810945f00bc80
JhojanV70 <jhojanv70@gmail.com>
2024-05-12 13:02:22 -0500

    Edit Products

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,7 +7,10 @@ namespace SupermarketWEB.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
         public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
